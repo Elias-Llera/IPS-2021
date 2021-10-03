a8de5fc4c165382c202d81a3363d31006ce425db
Jorge López Fueyo <jorge.lopez@neosentec.com>
2021-10-03 23:41:29 +0200

    Modificadas las clases sql y schme más dos clases nuevas

---
+++ samples-test-dev/src/main/java/giis/demo/tkrun/AtletaEntity.java
@@ -0,0 +1,69 @@
+package giis.demo.tkrun;
+
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AtletaEntity {
+	private String email;
+	private String name;
+	private String surname;
+	private String birthDate;
+	private boolean isMale;
+	
+	//Relaciones
+	private List<InscripcionEntity> inscripciones = new ArrayList<>();
+	
+	public AtletaEntity(String email, boolean isMale, String birthDate) {
+		this.setEmail(email);
+		this.setBirthDate(birthDate);
+		this.setMale(isMale);
+	}
+	
+	public void registrarse(InscripcionEntity inscripcion){
+		inscripciones.add(inscripcion);
+	}
+
+	
+	//Getters y setters
+	//NO TODOS SON NECESARIOS
+	public String getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(String birthDate) {
+		this.birthDate = birthDate;
+	}
+
+	public boolean isMale() {
+		return isMale;
+	}
+
+	public void setMale(boolean isMale) {
+		this.isMale = isMale;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
--- samples-test-dev/src/main/resources/schema.sql
+++ samples-test-dev/src/main/resources/schema.sql
@@ -2,48 +2,33 @@
 --(en este caso en cada una de las aplicaciones (tkrun y descuento) se usa solo una tabla, por lo que no hace falta)
 
 --Para giis.demo.tkrun:
+DROP TABLE IF EXISTS carreras;
+DROP TABLE IF EXISTS atletas;
+DROP TABLE IF EXISTS inscripciones;
 
---Eliminar tablas anteriores
-drop table Carreras;
-drop table Inscritos;
-drop table Atleta;
-drop table Categoria;
-
---Crear nuevas tablas
-create table Carrera (
-    id int primary key not null, 
-    nombre varchar not null, 
-    tipo ENUM('urbana', 'tierra') not null,
-    descr varchar(32), 
-    inicio_inscripcion date not null, 
-    fin_inscripcion date not null, 
-    precio_inscripcion float not null, 
-    fecha date not null, 
-    check(inicio<=fin), 
-    check(fin<fecha)
+CREATE TABLE carreras(
+	id INT PRIMARY KEY NOT NULL, 
+	inicio DATE NOT NULL, 
+	fin DATE NOT NULL, 
+	fecha DATE NOT NULL, 
+	descr VARCHAR(32), 
+	CHECK(inicio<=fin), 
+	CHECK(fin<fecha)
+);
+CREATE TABLE atletas(
+	email VARCHAR(32) PRIMARY KEY NOT NULL, 
+	name VARCHAR(32) NOT NULL, 
+	surname VARCHAR(32) NOT NULL, 
+	birthDATE DATE NOT NULL, 
+	isMale BOOLEAN NOT NULL
 );
 
-create table Atleta (
-    email varchar primary key not null, 
-    nombre varchar, 
-    apellido varchar, 
-    nacimiento date not null,
-    sexo ('male', 'female')
+CREATE table inscripciones(
+	id_carrera INT NOT NULL,
+	email_atleta VARCHAR(32) NOT NULL,
+	estado VARCHAR(32) NOT NULL,
+	dorsal INT NOT NULL,
+	PRIMARY KEY(id_carrera, email_atleta)
 );
 
-create table Inscritos(
-    email varchar foreign key references Atleta(email), 
-    id_carrera varchar foreign key references Carrera(id), 
-    primary key (email, id_carrera), 
-    estado ENUM('preinscrito', 'inscrito'), 
-    dorsal int
-);
 
-create table Categoria(
-    id int primary key not null,
-    id_carrera foreign key references Carrera(id),
-    edad_inicio int,
-    edad_final int,
-    sexo value ('male', 'female'),
-    nombre varchar
-);
